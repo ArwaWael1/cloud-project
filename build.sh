b093a694a9ea93f3d67967fd428d3d38670574e1
ArwaWael1 <93154963+ArwaWael1@users.noreply.github.com>
2023-05-28 21:24:43 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t <api-gateway> -f api-gateway.Dockerfile .
-docker build -t <account-service> -f account-service.Dockerfile .
-docker build -t <frontend-server> -f frontend-server.Dockerfile .
-docker build -t <inventory-service> -f inventory-service.Dockerfile .
+docker build -t <api-gateway> -f api-gateway .
+docker build -t <account-service> -f account-service .
+docker build -t <frontend-server> -f frontend-server .
+docker build -t <inventory-service> -f inventory-service .
